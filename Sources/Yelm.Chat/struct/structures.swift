44992b0da1ff31fbf8f613b59f755eaffc9f9738
Michael Safir <admin@yelm.io>
2021-02-02 00:33:00 +0300

    add messages structure

--- Sources/Yelm.Chat/struct/structures.swift
+++ Sources/Yelm.Chat/struct/structures.swift
@@ -1,19 +1,45 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Michael on 30.01.2021.
-//
+
 
 import Foundation
+import Photos
+import SwiftUI
+import UIKit
+import Combine
 
+public struct chat_message: Identifiable, Hashable {
 
+    public init(id: Int, user: chat_user = chat_user(id: 0), text: String = "", time: String = "", date: String = "", attachments: [String : String] = [:], asset: PHAsset? = nil) {
+        self.id = id
+        self.user = user
+        self.text = text
+        self.time = time
+        self.date = date
+        self.attachments = attachments
+        self.asset = asset
+    }
 
-public struct messages_structure: Identifiable, Hashable {
     public var id: Int
-    /// message
-    public var message: String = ""
-    /// images
-    public var images: String = ""
+    public var user: chat_user = chat_user(id: 0)
+    public var text: String = ""
+    public var time: String = ""
+    public var date: String = ""
+    public var attachments : [String : String] = [:]
+    public var asset : PHAsset? = nil
+}
 
+
+public struct chat_user: Identifiable, Hashable {
+
+    public init(id: Int, name: String = "") {
+        self.id = id
+        self.name = name
+    }
+
+    public var id: Int
+    public var name: String = ""
 }

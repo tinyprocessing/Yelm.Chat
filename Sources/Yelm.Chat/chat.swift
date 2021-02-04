35be518d033142ebf759adef9437fcbb789603ec
Michael Safir <admin@yelm.io>
2021-02-04 12:04:50 +0300

    optimization bottom scroll

--- Sources/Yelm.Chat/chat.swift
+++ Sources/Yelm.Chat/chat.swift
@@ -14,7 +14,7 @@ public class ChatEngine: ObservableObject, Identifiable{
     public var id: Int = 0
     @Published public var keyboard: CGFloat = 0
     @Published public var messages : [chat_message] = []
-    @Published public var keyboard_metric: Int = 0
+    @Published public var animation : Bool = false
     
     
     init() {
@@ -31,26 +31,48 @@ public class ChatEngine: ObservableObject, Identifiable{
             name: UIResponder.keyboardDidHideNotification,
             object: nil
         )
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboard_show),
+            name: UIResponder.keyboardDidShowNotification,
+            object: nil
+        )
     }
-
+    
     @objc func keyboard_will_show(_ notification: Notification) {
+        
         if let frame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
             let rect = frame.cgRectValue
-            
-            YelmChat.objectWillChange.send()
             keyboard = rect.height
-            print("будет показана")
+            
         }
     }
     
     @objc func keyboard_show(_ notification: Notification) {
-        YelmChat.objectWillChange.send()
-        self.keyboard_metric = 30
+        self.bottom()
+    
+        
     }
     
     @objc func keyboard_hide(_ notification: Notification) {
+        
+        
+        self.bottom()
+      
+    }
+    
+    public func bottom() {
+        
+        YelmChat.chat.animation = true
         YelmChat.objectWillChange.send()
-        self.keyboard_metric = 15
+        YelmChat.chat.messages.insert(chat_message(id: 1), at: 0)
+//
+//        DispatchQueue.main.asyncAfter(deadline: .now()+0.01) {
+//            YelmChat.objectWillChange.send()
+//            YelmChat.chat.messages.removeFirst()
+//        }
+        
     }
     
 }

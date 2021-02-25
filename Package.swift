94be7378b3e57759ac74a23dbeab0bfc92bf4be4
Michael Safir <admin@yelm.io>
2021-02-25 22:37:04 +0300

    Yelm.Server add

--- Package.swift
+++ Package.swift
@@ -21,14 +21,15 @@ let package = Package(
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0"),
         .package(url: "https://github.com/devicekit/DeviceKit.git", from: "4.0.0"),
         .package(name: "SocketIO", url: "https://github.com/socketio/socket.io-client-swift.git", .upToNextMinor(from: "15.2.0")),
-        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa.git", from: "10.5.0")
+        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa.git", from: "10.5.0"),
+        .package(name: "Yelm.Server", url: "https://github.com/Michaelsafir/Yelm.Server.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Yelm.Chat",
-            dependencies: ["Alamofire", "DeviceKit", "SwiftyJSON", .product(name: "RealmSwift", package: "Realm"), "SocketIO"]),
+            dependencies: ["Alamofire", "DeviceKit", "SwiftyJSON", .product(name: "RealmSwift", package: "Realm"), "SocketIO", "Yelm.Server"]),
         .testTarget(
             name: "Yelm.ChatTests",
             dependencies: ["Yelm.Chat"]),
